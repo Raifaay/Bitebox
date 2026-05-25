cae4b779a7dcfd557089195cfb0ce33a39716a49
Raifaay <252410102020@mail.unej.ac.id>
2026-05-25 22:31:13 +0700

    update halaman login bitebox

---
+++ Bitebox/Formregister.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Bitebox
+{
+    partial class Formregister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Formregister));
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(400, 114);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(499, 31);
+            textBox1.TabIndex = 0;
+            // 
+            // Formregister
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 249, 243);
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(1256, 619);
+            Controls.Add(textBox1);
+            Name = "Formregister";
+            Text = "Formregister";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+    }
+}
