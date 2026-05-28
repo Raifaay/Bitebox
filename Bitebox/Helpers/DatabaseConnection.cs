da20d708e8fd3811102eb822bca227f18961d45b
Raifaay <252410102020@mail.unej.ac.id>
2026-05-29 00:10:15 +0700

    database

---
+++ Bitebox/Helpers/DatabaseConnection.cs
@@ -0,0 +1,42 @@
+﻿using Npgsql;
+
+public class Database
+{
+    private string _connString;
+
+    public Database(string connString)
+    {
+        _connString = connString;
+    }
+
+    public NpgsqlConnection GetConnection()
+    {
+        return new NpgsqlConnection(_connString);
+    }
+}
+public static class Config
+{
+    public static string ConnString =
+        "Host=localhost;" +
+        "Port=5432;" +
+        "Database=praktikum-pbo;" +
+        "Username=postgres;" +
+        "Password=Password";
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            using (var conn = new Database().GetConnection())
+            {
+                conn.Open();
+                Console.WriteLine("Koneksi berhasil!");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
