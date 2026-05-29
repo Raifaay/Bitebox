225a960dac3cc69bfc8ac4676d4501f7c9df0eb5
Raifaay <252410102020@mail.unej.ac.id>
2026-05-29 18:38:33 +0700

    Menambahkan tombol close dan memperbaiki logika loginmodels

--- Bitebox/Views/formlogin.Designer.cs
+++ Bitebox/Views/formlogin.Designer.cs
@@ -28,20 +28,143 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            btnClose = new Button();
+            BtnLogin = new Button();
+            TbPassword = new TextBox();
+            TbUsername = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
             SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Location = new Point(89, 224);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(423, 422);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(btnClose);
+            panel1.Controls.Add(BtnLogin);
+            panel1.Controls.Add(TbPassword);
+            panel1.Controls.Add(TbUsername);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(53, 13);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1837, 1042);
+            panel1.TabIndex = 1;
+            // 
+            // btnClose
+            // 
+            btnClose.Font = new Font("Gill Sans Ultra Bold", 22F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnClose.ForeColor = Color.FromArgb(128, 64, 64);
+            btnClose.Location = new Point(1498, 130);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(66, 60);
+            btnClose.TabIndex = 7;
+            btnClose.Text = "X";
+            btnClose.UseVisualStyleBackColor = true;
+            btnClose.Click += btnClose_Click;
+            // 
+            // BtnLogin
+            // 
+            BtnLogin.BackColor = Color.DarkOrange;
+            BtnLogin.Font = new Font("Cooper Black", 26F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            BtnLogin.ForeColor = Color.White;
+            BtnLogin.Location = new Point(579, 756);
+            BtnLogin.Name = "BtnLogin";
+            BtnLogin.Size = new Size(655, 99);
+            BtnLogin.TabIndex = 6;
+            BtnLogin.Text = "Login";
+            BtnLogin.UseVisualStyleBackColor = false;
+            BtnLogin.Click += BtnLogin_Click;
+            // 
+            // TbPassword
+            // 
+            TbPassword.Location = new Point(755, 527);
+            TbPassword.Name = "TbPassword";
+            TbPassword.Size = new Size(392, 31);
+            TbPassword.TabIndex = 5;
+            // 
+            // TbUsername
+            // 
+            TbUsername.Location = new Point(751, 384);
+            TbUsername.Name = "TbUsername";
+            TbUsername.Size = new Size(396, 31);
+            TbUsername.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Cooper Black", 22F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.FromArgb(128, 64, 0);
+            label3.Location = new Point(739, 452);
+            label3.Name = "label3";
+            label3.Size = new Size(253, 50);
+            label3.TabIndex = 3;
+            label3.Text = "Password ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Cooper Black", 22F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(128, 64, 0);
+            label2.Location = new Point(739, 302);
+            label2.Name = "label2";
+            label2.Size = new Size(396, 50);
+            label2.TabIndex = 2;
+            label2.Text = "Email/Username";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Showcard Gothic", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ControlDark;
+            label1.Location = new Point(609, 224);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 44);
+            label1.TabIndex = 1;
             // 
             // FormLogin
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.NavajoWhite;
+            ClientSize = new Size(1920, 1080);
+            Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "FormLogin";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "FormLogin";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private Label label1;
+        private Label label3;
+        private Label label2;
+        private TextBox TbUsername;
+        private TextBox TbPassword;
+        private Button BtnLogin;
+        private Button btnClose;
     }
 }
--- Bitebox/Views/formlogin.cs
+++ Bitebox/Views/formlogin.cs
@@ -10,9 +10,35 @@ namespace Bitebox.Views
 {
     public partial class FormLogin : Form
     {
+        private string username;
+        private string password;
         public FormLogin()
         {
             InitializeComponent();
+            username = "aqila";
+            password = "aqila123"; 
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            // tes push ulang 
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            string usernameInput = TbUsername.Text;
+            string passwordInput = TbPassword.Text;
+
+            if (usernameInput == username && passwordInput == password)
+            {
+                MessageBox.Show("Login Berhasil", "success", MessageBoxButtons.OK, MessageBoxIcon.Information); 
+            }
+
+            else
+            {
+                MessageBox.Show("Username Atau Password Salah", "Gagal", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+            }
         }
     }
 }
