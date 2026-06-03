757b19bb95ddf39da2ad974fac60ae5d68e5cf6b
aqilafebryn2007@outlook.com <aqilafebryn2007@outlook.com>
2026-06-03 19:13:41 +0700

    update admin

--- Bitebox/Views/Admin/FormDashboard.Designer.cs
+++ Bitebox/Views/Admin/FormDashboard.Designer.cs
@@ -29,14 +29,70 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
+            btnDashboard = new Button();
+            btnPengelolaMenu = new Button();
+            btnLaporanPenjualan = new Button();
+            btnKelolaCustomer = new Button();
+            btnLogout = new Button();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
+            panel1.Controls.Add(btnLogout);
+            panel1.Controls.Add(btnKelolaCustomer);
+            panel1.Controls.Add(btnLaporanPenjualan);
+            panel1.Controls.Add(btnPengelolaMenu);
+            panel1.Controls.Add(btnDashboard);
             panel1.Location = new Point(3, 4);
             panel1.Name = "panel1";
             panel1.Size = new Size(287, 1017);
             panel1.TabIndex = 0;
+            // 
+            // btnDashboard
+            // 
+            btnDashboard.Location = new Point(38, 200);
+            btnDashboard.Name = "btnDashboard";
+            btnDashboard.Size = new Size(197, 57);
+            btnDashboard.TabIndex = 0;
+            btnDashboard.Text = "Dashboard";
+            btnDashboard.UseVisualStyleBackColor = true;
+            // 
+            // btnPengelolaMenu
+            // 
+            btnPengelolaMenu.Location = new Point(38, 300);
+            btnPengelolaMenu.Name = "btnPengelolaMenu";
+            btnPengelolaMenu.Size = new Size(197, 59);
+            btnPengelolaMenu.TabIndex = 1;
+            btnPengelolaMenu.Text = "Pengelola Menu ";
+            btnPengelolaMenu.UseVisualStyleBackColor = true;
+            // 
+            // btnLaporanPenjualan
+            // 
+            btnLaporanPenjualan.Location = new Point(38, 395);
+            btnLaporanPenjualan.Name = "btnLaporanPenjualan";
+            btnLaporanPenjualan.Size = new Size(196, 48);
+            btnLaporanPenjualan.TabIndex = 2;
+            btnLaporanPenjualan.Text = "Laporan Penjualan";
+            btnLaporanPenjualan.UseVisualStyleBackColor = true;
+            // 
+            // btnKelolaCustomer
+            // 
+            btnKelolaCustomer.Location = new Point(38, 481);
+            btnKelolaCustomer.Name = "btnKelolaCustomer";
+            btnKelolaCustomer.Size = new Size(188, 50);
+            btnKelolaCustomer.TabIndex = 3;
+            btnKelolaCustomer.Text = "Kelola Customer";
+            btnKelolaCustomer.UseVisualStyleBackColor = true;
+            // 
+            // btnLogout
+            // 
+            btnLogout.Location = new Point(38, 574);
+            btnLogout.Name = "btnLogout";
+            btnLogout.Size = new Size(161, 43);
+            btnLogout.TabIndex = 4;
+            btnLogout.Text = "Logout";
+            btnLogout.UseVisualStyleBackColor = true;
             // 
             // FormDashboard
             // 
@@ -48,11 +104,17 @@
             Name = "FormDashboard";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "FormDashboard";
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel panel1;
+        private Button btnDashboard;
+        private Button btnLogout;
+        private Button btnKelolaCustomer;
+        private Button btnLaporanPenjualan;
+        private Button btnPengelolaMenu;
     }
 }
