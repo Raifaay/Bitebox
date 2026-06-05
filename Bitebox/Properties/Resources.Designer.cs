7d215314e4ebc68e38a690dc5d8ce625ba3b3299
Raifaay <252410102020@mail.unej.ac.id>
2026-06-05 12:47:25 +0700

    updattee

--- Bitebox/Properties/Resources.Designer.cs
+++ Bitebox/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace Bitebox.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap btn {
+            get {
+                object obj = ResourceManager.GetObject("btn", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap burger {
             get {
                 object obj = ResourceManager.GetObject("burger", resourceCulture);
